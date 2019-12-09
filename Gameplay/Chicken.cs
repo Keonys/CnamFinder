b2103cc3394afaec25e1f82446648c3fbc349ddb
Keonys <keonys68@hotmail.fr>
2019-12-09 17:20:41 +0100

    adding factory design pattern and fix

---
+++ Gameplay/Chicken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPGM.Gameplay;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace RPGM.Gameplay
+{
+    /// <summary>
+    /// Define a chicken item
+    /// </summary>
+    public class Chicken : InventoryItem
+    {
+
+    }
+}
